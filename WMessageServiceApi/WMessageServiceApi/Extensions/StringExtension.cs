6c83421a42d635ea823f54426d1c265c1b93c03e
zeroJava <zero_2009@hotmail.co.uk>
2024-02-10 01:13:09 +0000

    cleanup

--- WMessageServiceApi/WMessageServiceApi/Extensions/StringExtension.cs
+++ WMessageServiceApi/WMessageServiceApi/Extensions/StringExtension.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WMessageServiceApi.Extensions
+﻿namespace WMessageServiceApi.Extensions
 {
 	public static class StringExtension
 	{
